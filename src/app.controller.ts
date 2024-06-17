a8247425a4a2a8b5637dcb4f67d13c1d4a425ff4
madhusudhan1909 <124492728+madhusudhan1909@users.noreply.github.com>
2024-06-17 22:47:35 +0530

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,6 +4,6 @@ import { Controller, Get } from '@nestjs/common';
 export class AppController {
   @Get()
   getHello(): string {
-    return 'Hello World32!'; // Ensure this method returns 'Hello World!'
+    return 'Hello World!'; // Ensure this method returns 'Hello World!'
   }
 }
